5c6b6c9281d2398045e257e2276ef143f9c10a59
Sam <sam@moocows.co.uk>
2025-01-02 10:18:13 +0000

    Refactored sheet variables to use constants and made the import error hang for longer

--- TheMagic.py
+++ TheMagic.py
@@ -1,9 +1,11 @@
 # IMPORTS
 try:
+    from time import sleep
     from os import listdir
     import pandas as pd
 except ImportError:
     print("Pandas not installed. Please install pandas using 'pip install pandas'")
+    sleep(5)
     exit()
 
 # Access Data
@@ -38,19 +40,22 @@ def find_min(files, category):
         data = pd.read_csv(file)
         for i in data[category]:
             if i == "tr":
-                narrow_min.append(0)
+                narrow_min.append("0")
             else:
                 narrow_min.append(i)
         wide_min.append(min(narrow_min))
     return min(wide_min)
 
 
+UK_VAR = "Daily Total Rainfall (0900-0900) (mm)"
+GLOB_VAR = "Rainfall (24 hour total)"
+
 # Find the max temperature for each location
-UK_max_temp = find_max(uk_files, "Daily Mean Temperature (0900-0900) (°C)")
-GLOBE_max_temp = find_max(foreign_files, "Daily Mean Air Temperature")
+UK_max_temp = find_max(uk_files, UK_VAR)
+GLOBE_max_temp = find_max(foreign_files, GLOB_VAR)
 print(f"UK MAX: {UK_max_temp}\nGLOBE MAX: {GLOBE_max_temp}")
 
 # Find the min temperature for each location
-UK_min_temp = find_min(uk_files, "Daily Mean Temperature (0900-0900) (°C)")
-GLOBE_min_temp = find_min(foreign_files, "Daily Mean Air Temperature")
+UK_min_temp = find_min(uk_files, UK_VAR)
+GLOBE_min_temp = find_min(foreign_files, GLOB_VAR)
 print(f"UK MIN: {UK_min_temp}\nGLOBE MIN: {GLOBE_min_temp}")
